6cb7086cbaf7c84e507793c25468780ae3dcd954
Danny Wood <danny@inspiredagency.co.uk>
2020-06-16 19:12:27 +0100

    Add functionality to refresh badge image every 2 minutes once deploy is triggered. Add message function so feedback can be displayed to the user.

--- index.js
+++ index.js
@@ -6,25 +6,32 @@ function init(config = {}) {
 
   const badge = initBadge(config.badge_url);
 
-  container.appendChild(link);
+  const message = initMessage();
+
+  container.appendChild(message);
   badge && container.appendChild(badge);
+  container.appendChild(link);
   document.body.appendChild(container);
 }
 
 module.exports.netlifyPublish = init;
 
+// Container
 function initContainer() {
   const container = document.createElement("div");
+  container.id = "netlify-publish-container";
   container.style.cssText = defaultContainerStyle();
-  container.innerText = "Deploy Site: ";
 
   return container;
 }
+// ---
 
+// Link
 function initLink() {
   const link = document.createElement("a");
   link.text = "Publish";
   link.href = "/";
+  link.style.cssText = defaultLinkStyle();
 
   return link;
 }
@@ -39,34 +46,66 @@ function bindDeployClickEvent(link, build_url) {
     deploySite(build_url);
   });
 }
+// ---
 
-async function deploySite(build_url) {
-  try {
-    console.log("Publishing Site:", build_url);
-
-    const request = await fetch(build_url, {
-      method: "POST",
-    });
-
-    refreshBadge();
-  } catch (ex) {
-    console.error(ex.message);
-  }
-}
-
+// Badge
 function initBadge(badge_url) {
   const badge = document.createElement("img");
   badge.src = badge_url;
   badge.id = "netlify_publish__badge";
+  badge.style.cssText = defaultBadgeStyle();
 
   return badge;
 }
 
+function initBadgeRefresh() {
+  refreshBadge();
+  setInterval(refreshBadge, 120000); // Refresh badge every 2 minutes once triggered
+}
+
 function refreshBadge() {
+  console.log("Refreshing Badge");
   const badge = document.getElementById("netlify_publish__badge");
   badge.src = badge.src + "?" + new Date().getTime();
 }
+// ---
 
+// Message
+function initMessage() {
+  const message = document.createElement("span");
+  message.id = "netlify-publish-message";
+  message.style.cssText = defaultMessageStyle();
+
+  return message;
+}
+
+function showMessage(text) {
+  const message = document.getElementById("netlify-publish-message");
+  message.innerText = text;
+
+  setTimeout(function () {
+    message.innerText = "";
+  }, 5000);
+}
+//--
+
+// Deploy
+async function deploySite(build_url) {
+  try {
+    showMessage("Deploying Site...");
+
+    const response = await fetch(build_url, {
+      method: "POST",
+    });
+
+    response.ok && initBadgeRefresh();
+  } catch (ex) {
+    console.error(ex.message);
+  }
+}
+// ---
+
+// Styling
 function defaultContainerStyle() {
   return `
           font-family: Arial;
@@ -75,7 +114,37 @@ function defaultContainerStyle() {
           position: fixed;
           bottom: 0;
           left:0;
-          width: 100%;
-          padding: 10px;
+          width: calc(100% - 30px);
+          padding: 10px 15px;
+          text-align: right;
       `;
 }
+
+function defaultLinkStyle() {
+  return `
+          font-family: Arial;
+          background: #009387;
+          color: #ffffff;
+          padding: 10px;
+          display: inline-block;
+          text-decoration: none;
+          margin-left: 15px;
+      `;
+}
+
+function defaultBadgeStyle() {
+  return `
+          margin-bottom: -5px;
+      `;
+}
+
+function defaultMessageStyle() {
+  return `
+          font-family: Arial;
+          color: #009387;
+          font-size: 16px;
+          float: left;
+          margin-top: 10px;
+      `;
+}
+// ---
